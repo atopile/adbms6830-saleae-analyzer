35bf16d629a2f70eab35e7e85f2c709a165bc4ef
Nicholas Krstevski <nick@atopile.io>
2025-08-28 20:36:38 -0700

    working like a charm

--- HighLevelAnalyzer.py
+++ HighLevelAnalyzer.py
@@ -1,5 +1,6 @@
 # High Level Analyzer
 # For more information and documentation, please go to https://support.saleae.com/extensions/high-level-analyzer-extensions
+from typing import Union, Tuple, Optional, List
 
 from saleae.analyzers import (
     HighLevelAnalyzer,
@@ -8,7 +9,6 @@ from saleae.analyzers import (
     NumberSetting,
     ChoicesSetting,
 )
-
 
 def parse_voltage(data: bytearray) -> str:
     return f"{(int.from_bytes(bytes(data), byteorder='little', signed=True) * 0.000150) + 1.5:.3f}V"
@@ -72,10 +72,15 @@ REGISTER_MAP = {
     0x0017: {"name": "RDFCF", "parse_fn": print_bytes},
     0x0018: {"name": "RDFCALL", "parse_fn": print_bytes},
     # AUX Registers
-    0x0019: {"name": "RDAUXA", "parse_fn": print_bytes},
-    0x001A: {"name": "RDAUXB", "parse_fn": print_bytes},
-    0x001B: {"name": "RDAUXC", "parse_fn": print_bytes},
-    0x001F: {"name": "RDAUXD", "parse_fn": print_bytes},
+    0x0019: {"name": "RDAUXA", "parse_fn": parse_voltage, "offset": 0},
+    0x001A: {"name": "RDAUXB", "parse_fn": parse_voltage, "offset": 3},
+    0x001B: {"name": "RDAUXC", "parse_fn": parse_voltage, "offset": 6},
+    0x001F: {"name": "RDAUXD", "parse_fn": parse_voltage, "offset": 9},
+    # AUX Registers
+    0x001C: {"name": "RDRAXA", "parse_fn": parse_voltage, "offset": 0},
+    0x001D: {"name": "RDRAXB", "parse_fn": parse_voltage, "offset": 3},
+    0x001E: {"name": "RDRAXC", "parse_fn": parse_voltage, "offset": 6},
+    0x0025: {"name": "RDRAXD", "parse_fn": parse_voltage, "offset": 9},
     # ID Register
     0x002C: {"name": "RDSID", "parse_fn": print_bytes},
     # Balance PWM Registers
@@ -84,6 +89,81 @@ REGISTER_MAP = {
     0x0021: {"name": "WRPWMB", "parse_fn": print_bytes},
     0x0023: {"name": "RDPWMB", "parse_fn": print_bytes},
 }
+
+
+def calculatePEC15(pDataBuf: Union[bytes, bytearray]) -> int:
+    nRemainder = 16  # initialize the PEC
+    for b in pDataBuf:
+        nTableAddr = ((nRemainder >> 7) ^ (b & 0xFF)) & 0xFF
+        nRemainder = ((nRemainder << 8) ^ Adbms6948_Crc15Table[nTableAddr]) & 0xFFFF
+    # The CRC15 has a 0 in the LSB so the remainder must be multiplied by 2
+    return (nRemainder * 2) & 0xFFFF
+
+
+def calculatePEC10(pDataBuf: Union[bytes, bytearray], bIsRxCmd: bool, counter_byte: Optional[int] = None) -> int:
+    nRemainder = 16  # PEC_SEED
+    # x10 + x7 + x3 + x2 + x + 1 <- the CRC10 polynomial 100 1000 1111
+    nPolynomial = 0x8F
+    for b in pDataBuf:
+        nTableAddr = ((nRemainder >> 2) ^ (b & 0xFF)) & 0xFF
+        nRemainder = ((nRemainder << 8) ^ Adbms6948_Crc10Table[nTableAddr]) & 0xFFFF
+    # If array is from received buffer add command counter to crc calculation
+    if bIsRxCmd and (counter_byte is not None):
+        nRemainder ^= ((counter_byte & 0xFC) << 2) & 0xFFFF
+    # Perform modulo-2 division, a bit at a time
+    for _ in range(6, 0, -1):
+        # Try to divide the current data bit
+        if (nRemainder & 0x200) > 0:
+            nRemainder = ((nRemainder << 1) ^ nPolynomial) & 0xFFFF
+        else:
+            nRemainder = (nRemainder << 1) & 0xFFFF
+    return nRemainder & 0x3FF
+
+
+Adbms6948_Crc15Table: Tuple[int, ...] = (
+    0x0000, 0xc599, 0xceab, 0x0b32, 0xd8cf, 0x1d56, 0x1664, 0xd3fd, 0xf407, 0x319e, 0x3aac,
+    0xff35, 0x2cc8, 0xe951, 0xe263, 0x27fa, 0xad97, 0x680e, 0x633c, 0xa6a5, 0x7558, 0xb0c1,
+    0xbbf3, 0x7e6a, 0x5990, 0x9c09, 0x973b, 0x52a2, 0x815f, 0x44c6, 0x4ff4, 0x8a6d, 0x5b2e,
+    0x9eb7, 0x9585, 0x501c, 0x83e1, 0x4678, 0x4d4a, 0x88d3, 0xaf29, 0x6ab0, 0x6182, 0xa41b,
+    0x77e6, 0xb27f, 0xb94d, 0x7cd4, 0xf6b9, 0x3320, 0x3812, 0xfd8b, 0x2e76, 0xebef, 0xe0dd,
+    0x2544, 0x02be, 0xc727, 0xcc15, 0x098c, 0xda71, 0x1fe8, 0x14da, 0xd143, 0xf3c5, 0x365c,
+    0x3d6e, 0xf8f7, 0x2b0a, 0xee93, 0xe5a1, 0x2038, 0x07c2, 0xc25b, 0xc969, 0x0cf0, 0xdf0d,
+    0x1a94, 0x11a6, 0xd43f, 0x5e52, 0x9bcb, 0x90f9, 0x5560, 0x869d, 0x4304, 0x4836, 0x8daf,
+    0xaa55, 0x6fcc, 0x64fe, 0xa167, 0x729a, 0xb703, 0xbc31, 0x79a8, 0xa8eb, 0x6d72, 0x6640,
+    0xa3d9, 0x7024, 0xb5bd, 0xbe8f, 0x7b16, 0x5cec, 0x9975, 0x9247, 0x57de, 0x8423, 0x41ba,
+    0x4a88, 0x8f11, 0x057c, 0xc0e5, 0xcbd7, 0x0e4e, 0xddb3, 0x182a, 0x1318, 0xd681, 0xf17b,
+    0x34e2, 0x3fd0, 0xfa49, 0x29b4, 0xec2d, 0xe71f, 0x2286, 0xa213, 0x678a, 0x6cb8, 0xa921,
+    0x7adc, 0xbf45, 0xb477, 0x71ee, 0x5614, 0x938d, 0x98bf, 0x5d26, 0x8edb, 0x4b42, 0x4070,
+    0x85e9, 0x0f84, 0xca1d, 0xc12f, 0x04b6, 0xd74b, 0x12d2, 0x19e0, 0xdc79, 0xfb83, 0x3e1a, 0x3528,
+    0xf0b1, 0x234c, 0xe6d5, 0xede7, 0x287e, 0xf93d, 0x3ca4, 0x3796, 0xf20f, 0x21f2, 0xe46b, 0xef59,
+    0x2ac0, 0x0d3a, 0xc8a3, 0xc391, 0x0608, 0xd5f5, 0x106c, 0x1b5e, 0xdec7, 0x54aa, 0x9133, 0x9a01,
+    0x5f98, 0x8c65, 0x49fc, 0x42ce, 0x8757, 0xa0ad, 0x6534, 0x6e06, 0xab9f, 0x7862, 0xbdfb, 0xb6c9,
+    0x7350, 0x51d6, 0x944f, 0x9f7d, 0x5ae4, 0x8919, 0x4c80, 0x47b2, 0x822b, 0xa5d1, 0x6048, 0x6b7a,
+    0xaee3, 0x7d1e, 0xb887, 0xb3b5, 0x762c, 0xfc41, 0x39d8, 0x32ea, 0xf773, 0x248e, 0xe117, 0xea25,
+    0x2fbc, 0x0846, 0xcddf, 0xc6ed, 0x0374, 0xd089, 0x1510, 0x1e22, 0xdbbb, 0x0af8, 0xcf61, 0xC453,
+    0x01ca, 0xd237, 0x17ae, 0x1c9c, 0xd905, 0xfeff, 0x3b66, 0x3054, 0xf5cd, 0x2630, 0xe3a9, 0xe89b,
+    0x2d02, 0xa76f, 0x62f6, 0x69c4, 0xac5d, 0x7fa0, 0xba39, 0xb10b, 0x7492, 0x5368, 0x96f1, 0x9dc3,
+    0x585a, 0x8ba7, 0x4e3e, 0x450c, 0x8095,
+)
+
+Adbms6948_Crc10Table: Tuple[int, ...] = (
+    0x000, 0x08f, 0x11e, 0x191, 0x23c, 0x2b3, 0x322, 0x3ad, 0x0f7, 0x078, 0x1e9, 0x166, 0x2cb, 0x244, 0x3d5, 0x35a,
+    0x1ee, 0x161, 0x0f0, 0x07f, 0x3d2, 0x35d, 0x2cc, 0x243, 0x119, 0x196, 0x007, 0x088, 0x325, 0x3aa, 0x23b, 0x2b4,
+    0x3dc, 0x353, 0x2c2, 0x24d, 0x1e0, 0x16f, 0x0fe, 0x071, 0x32b, 0x3a4, 0x235, 0x2ba, 0x117, 0x198, 0x009, 0x086,
+    0x232, 0x2bd, 0x32c, 0x3a3, 0x00e, 0x081, 0x110, 0x19f, 0x2c5, 0x24a, 0x3db, 0x354, 0x0f9, 0x076, 0x1e7, 0x168,
+    0x337, 0x3b8, 0x229, 0x2a6, 0x10b, 0x184, 0x015, 0x09a, 0x3c0, 0x34f, 0x2de, 0x251, 0x1fc, 0x173, 0x0e2, 0x06d,
+    0x2d9, 0x256, 0x3c7, 0x348, 0x0e5, 0x06a, 0x1fb, 0x174, 0x22e, 0x2a1, 0x330, 0x3bf, 0x012, 0x09d, 0x10c, 0x183,
+    0x0eb, 0x064, 0x1f5, 0x17a, 0x2d7, 0x258, 0x3c9, 0x346, 0x01c, 0x093, 0x102, 0x18d, 0x220, 0x2af, 0x33e, 0x3b1,
+    0x105, 0x18a, 0x01b, 0x094, 0x339, 0x3b6, 0x227, 0x2a8, 0x1f2, 0x17d, 0x0ec, 0x063, 0x3ce, 0x341, 0x2d0, 0x25f,
+    0x2e1, 0x26e, 0x3ff, 0x370, 0x0dd, 0x052, 0x1c3, 0x14c, 0x216, 0x299, 0x308, 0x387, 0x02a, 0x0a5, 0x134, 0x1bb,
+    0x30f, 0x380, 0x211, 0x29e, 0x133, 0x1bc, 0x02d, 0x0a2, 0x3f8, 0x377, 0x2e6, 0x269, 0x1c4, 0x14b, 0x0da, 0x055,
+    0x13d, 0x1b2, 0x023, 0x0ac, 0x301, 0x38e, 0x21f, 0x290, 0x1ca, 0x145, 0x0d4, 0x05b, 0x3f6, 0x379, 0x2e8, 0x267,
+    0x0d3, 0x05c, 0x1cd, 0x142, 0x2ef, 0x260, 0x3f1, 0x37e, 0x024, 0x0ab, 0x13a, 0x1b5, 0x218, 0x297, 0x306, 0x389,
+    0x1d6, 0x159, 0x0c8, 0x047, 0x3ea, 0x365, 0x2f4, 0x27b, 0x121, 0x1ae, 0x03f, 0x0b0, 0x31d, 0x392, 0x203, 0x28c,
+    0x038, 0x0b7, 0x126, 0x1a9, 0x204, 0x28b, 0x31a, 0x395, 0x0cf, 0x040, 0x1d1, 0x15e, 0x2f3, 0x27c, 0x3ed, 0x362,
+    0x20a, 0x285, 0x314, 0x39b, 0x036, 0x0b9, 0x128, 0x1a7, 0x2fd, 0x272, 0x3e3, 0x36c, 0x0c1, 0x04e, 0x1df, 0x150,
+    0x3e4, 0x36b, 0x2fa, 0x275, 0x1d8, 0x157, 0x0c6, 0x049, 0x313, 0x39c, 0x20d, 0x282, 0x12f, 0x1a0, 0x031, 0x0be,
+)
 
 
 # High level analyzers must subclass the HighLevelAnalyzer class.
@@ -95,8 +175,8 @@ class Hla(HighLevelAnalyzer):
 
     # An optional list of types this analyzer produces, providing a way to customize the way frames are displayed in Logic 2.
     result_types = {
-        "Command": {"format": "{{data.command}}"},
-        "PEC": {"format": "PEC: {{data.pec}}"},
+        "Command": {"format": "{{data.command}}",},
+        "PEC": {"format": "{{data.pec}}"},
         "Data": {"format": "{{data.data}}"},
     }
 
@@ -143,7 +223,7 @@ class Hla(HighLevelAnalyzer):
             self.frames.append(frame)
 
     def get_frame_data(self) -> list:
-        analyzer_frames: list[AnalyzerFrame] = []
+        analyzer_frames: List[AnalyzerFrame] = []
 
         if len(self.frames) < 4:
             return []
@@ -166,9 +246,10 @@ class Hla(HighLevelAnalyzer):
         command = bytes(mosi_stream[0:2])
         cmd_val = int.from_bytes(command, byteorder="big", signed=False)
         cmd_entry = REGISTER_MAP.get(cmd_val, {})
-        command_label = (
-            cmd_entry.get("name") if isinstance(cmd_entry, dict) else f"0x{cmd_val:04X}"
-        )
+        if isinstance(cmd_entry, dict) and ("name" in cmd_entry):
+            command_label = cmd_entry["name"]
+        else:
+            command_label = f"0x{cmd_val:04X}"
 
         analyzer_frames.append(
             AnalyzerFrame(
@@ -185,6 +266,8 @@ class Hla(HighLevelAnalyzer):
         command_pec = bytes(mosi_stream[2:4])
         pec_val = int.from_bytes(command_pec, byteorder="big", signed=False)
         command_pec_label = f"0x{pec_val:04X}"
+        command_pec_calc = calculatePEC15(command)
+        pec_valid = command_pec_calc == pec_val
 
         analyzer_frames.append(
             AnalyzerFrame(
@@ -192,7 +275,7 @@ class Hla(HighLevelAnalyzer):
                 self.frames[2].start_time,
                 self.frames[3].end_time,
                 {
-                    "pec": command_pec_label,
+                    "pec": f"{command_pec_label} {'(VALID)' if pec_valid else '(INVALID)'}"
                 },
             )
         )
@@ -211,7 +294,10 @@ class Hla(HighLevelAnalyzer):
         parse_fn = cmd_entry.get("parse_fn") if isinstance(cmd_entry, dict) else None
 
         for asic_index, group in enumerate(self.data_frame_groups):
-            data_stream = miso_stream[4 + asic_index * 8 :]
+            if command_label.startswith("RD"):
+                data_stream = miso_stream[4 + asic_index * 8 :]
+            else:
+                data_stream = mosi_stream[4 + asic_index * 8 :]
             for data_index in [1, 3, 5]:
                 cell_index = (data_index - 1) / 2
                 if callable(parse_fn):
@@ -227,19 +313,30 @@ class Hla(HighLevelAnalyzer):
                         group[data_index - 1].start_time,
                         group[data_index].end_time,
                         {
-                            "data": f"A{asic_index + 1}C{cmd_entry.get('offset', 0) + cell_index}: {parsed_data}"
+                            "data": f"A{asic_index + 1}id{cmd_entry.get('offset', 0) + cell_index+1}: {parsed_data}"
                         },
                     )
                 )
 
-            data_pec = print_bytes(data_stream[6:8])
+            data_pec_bytes = data_stream[6:8]
+            # Show PEC as big-endian 16-bit for readability
+            data_pec_be_str = f"0x{int.from_bytes(data_pec_bytes, byteorder='big', signed=False):04X}"
+            # Extract counter and CRC10 from the two PEC bytes
+            rx_counter_byte = data_pec_bytes[0]
+            rx_crc10 = ((rx_counter_byte & 0x03) << 8) | data_pec_bytes[1]
+            cc_value = (rx_counter_byte & 0xFC) >> 2
+            # Compute expected CRC10 over the 6 data bytes + counter contribution
+            data_pec_calc = calculatePEC10(data_stream[:6], bIsRxCmd=True, counter_byte=rx_counter_byte)
+            data_pec_valid = (data_pec_calc == rx_crc10)
 
             analyzer_frames.append(
                 AnalyzerFrame(
                     "PEC",
                     group[6].start_time,
                     group[7].end_time,
-                    {"pec": data_pec},
+                    {
+                        "pec": f"CC: {cc_value} PEC: {data_pec_be_str} {'(VALID)' if data_pec_valid else f'(INVALID) calc 0x{data_pec_calc:03X}'}"
+                    },
                 )
             )
 
